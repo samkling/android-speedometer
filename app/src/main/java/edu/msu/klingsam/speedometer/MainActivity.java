c3c7f9c5c25ed4ce8591d4a2b778c6ce7a569d5e
samkling <klingtennis@gmail.com>
2016-12-19 15:23:08 -0500

    set the permissions in android manifest and onCreate to request location updates

--- app/src/main/java/edu/msu/klingsam/speedometer/MainActivity.java
+++ app/src/main/java/edu/msu/klingsam/speedometer/MainActivity.java
@@ -1,13 +1,21 @@
 package edu.msu.klingsam.speedometer;
 
+import android.content.Context;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,500,1,this);
+
+       // onLocationChanged(null);
     }
 }
