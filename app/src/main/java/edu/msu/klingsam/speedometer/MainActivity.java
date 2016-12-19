a385eff31e7779058d9567539aa0eea266925b80
samkling <klingtennis@gmail.com>
2016-12-19 15:04:09 -0500

    project push

---
+++ app/src/main/java/edu/msu/klingsam/speedometer/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.msu.klingsam.speedometer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
